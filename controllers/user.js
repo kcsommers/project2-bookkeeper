e9fc82762e827c8300b38cd12787991b34954d4f
Mark Sommers <kacysommers@gmail.com>
2018-06-28 15:01:50 -0700

    styled profile page

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var isLoggedIn = require('../middleware/isLoggedIn');
 var db = require('../models');
 var cloudinary = require('cloudinary');
+var multer = require('multer');
+var upload = multer({dest: '../uploads/'});
 
 
 // GET /user/profile - gets users profile page
@@ -19,14 +21,45 @@ router.get('/profile', isLoggedIn, function(req, res) {
 					groups: groups,
 					cloudinary: cloudinary
 				});
-			})
+			});
 		});
 	});
 });
 
-// GET /user/:id/update - update user info
+// GET /user/:id/update - get update user page
 router.get('/:id/update', function(req, res) {
-	res.render('user/updateUser', {user: req.user});
+	res.render('user/updateUser', {user: req.user, cloudinary: cloudinary});
+});
+
+// POST /user/:id/update - update user
+router.post('/:id/update', upload.single('userPic'), function(req, res) {
+	let imgUrl;
+	if(req.file) {
+		cloudinary.uploader.upload(req.file.path, function(result) {
+			imgUrl = result.public_id;
+			db.user.update({
+				name: req.body.name,
+				email: req.body.email,
+				location: req.body.location,
+				imgUrl: imgUrl
+			}, {
+				where: {id: req.user.id}
+			}).then(function(data) {
+				res.redirect('/suser/profile');
+			});
+		});
+	}
+	else {
+		db.user.update({
+			name: req.body.name,
+			email: req.body.email,
+			location: req.body.location
+		}, {
+			where: {id: req.user.id}
+		}).then(function(data) {
+			res.redirect('/user/profile');
+		})
+	}
 });
 
 module.exports = router;
--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
 	console.log('DOC READY');
-	// initialize materialize select form
+	// initialize materialize select form and modals
 	$('select').formSelect();
+	$('.modal').modal();
+
+	// list div click 
+	$('.list').click(function(e) {
+		let url = $(this).attr('data-url');
+		window.location = url;
+	});
+
+	// group click
+	$('.group-container').click(function(e) {
+		let url = $(this).attr('data-url');
+		window.location = url;
+	});
 
 	// delete lists
 	$('.delete-list-btn').click(function(e) {
@@ -14,6 +27,12 @@ $(document).ready(function() {
 		}).done(function(data) {
 			window.location = '/user/profile';
 		})
+	});
+	$('.delete-modal-trigger').on('click', function(e) {
+		e.stopPropagation();
+		let listIndex = $(this).attr('id');
+		var instance = M.Modal.getInstance($('#delete-list-modal-' + listIndex));
+		instance.open();
 	});
 
 	// delete book
